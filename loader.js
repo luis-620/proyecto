76022e6b4d1af2681212ef951091c6f46dc6db94
luis-620 <luis.yugsi5394@utc.edu.ec>
2026-08-02 13:08:42 -0500

    parte1

---
+++ loader.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",()=>{
+
+setTimeout(()=>{
+
+document.getElementById("loader").style.display="none";
+
+},3200);
+
+});
---
+++ main.js
@@ -0,0 +1,1 @@
+console.log("Boruto Web iniciada");
